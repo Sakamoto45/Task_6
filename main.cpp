54c89a4dc3cd41709efba810eb444ec67862180c
Sakamoto45 <57772310+Sakamoto45@users.noreply.github.com>
2020-06-07 15:47:47 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include "stack.hpp"
+#include <iostream>
+#include <string>
+#include <sstream>
+#define INF 1e9
+
+using namespace std;
+
+double calc(char op, double a, double b, string &opn) {
+    opn = opn + op + " ";
+    if (op == '*') return a * b;
+    if (op == '+') return a + b;
+    if (op == '-') return a - b;
+    if (op == '/' && b != 0) return a / b;
+    std::cout << "division by 0!\n";
+    return INF;
+}
+
+int main() {
+    stack<double> x;
+    stack<char> y;
+
+    char tmp;
+    char tmp_s;
+    char tmp_last = 's';//start
+    string opn = "";
+
+    string stream;
+    getline(cin, stream);
+    stringstream fin(stream);
+
+    while (!fin.eof()) {
+        fin >> tmp;
+        string num = "";
+        while (((tmp >= '0' && tmp <= '9') || ((tmp_last == '(' || tmp_last == 's') && tmp == '-') || (tmp == 'e' || tmp == ',' || (tmp_last == 'e' && (tmp == '-' || tmp == '+')))) && !fin.eof()) {
+            num += (tmp == ',' ? '.' : tmp);
+            tmp_last = tmp;
+            fin >> tmp;
+        }
+
+        if (num != "") {
+            x.push(stof(num));
+            opn += num + " ";
+            num = "";
+        }
+
+        if (tmp == '(' || tmp == '+' || tmp == '-' || tmp == '*' || tmp == '/') {
+            tmp_s = tmp;
+        
+            while (!y.cmp(tmp_s)) {
+                x.push(calc(y.pop(), x.pop(), x.pop(), opn));
+                if (x.top() == INF) return 0;
+            }
+            y.push(tmp_s);
+        }
+        else if (tmp == ')') {
+            tmp_s = y.pop();
+            while (tmp_s != '(') {
+                x.push(calc(tmp_s, x.pop(), x.pop(), opn));
+                if (x.top() == INF) return 0;
+                tmp_s = y.pop();
+            }
+
+        }
+        else if (tmp == '=') {
+            while (!y.is_empty()) {
+                x.push(calc(y.pop(), x.pop(), x.pop(), opn));
+                if (x.top() == INF) return 0;
+            }
+        }
+        tmp_last = tmp;
+    }
+    opn += tmp;
+    cout << x.pop() << endl;
+    cout << opn << endl;
+    return 0;
+}
+
+// "7+((6+2)/(1+1)-3)*(9+(2+5)*2)="
---
+++ stack.hpp
@@ -0,0 +1,82 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template<class T> class stack_elem {
+public:
+    T n;
+    stack_elem* next;
+};
+
+template<class T> class stack {
+public:
+
+    stack_elem<T>* list;
+
+    stack() {
+        list = NULL;
+    };
+
+    void push(T x) {
+        if (list == NULL) {
+            list = new stack_elem<T>();
+            list->n = x;
+        }
+
+        else {
+            stack_elem<T>* tmp = new stack_elem<T>;
+            tmp->n = x;
+            tmp->next = list;
+            list = tmp;
+        }
+    };
+
+    T pop() {
+        T x = list->n;
+        list = list->next;
+        return x;
+    };
+
+    T top() {
+        return list->n;
+    };
+
+    void print() {
+        stack_elem<T>* tmp = list;
+        while (tmp != NULL) {
+            cout << tmp->n << " ";
+            tmp = tmp->next;
+        }
+        cout << endl;
+    };
+    
+    bool cmp(const T& x) {
+        if (list == NULL || list->n == '(' || x == '(')
+            return true;
+        int a = 0, b = 0;
+
+        switch ((list->n)) {
+            case '(': a++;
+            case '*':
+            case '/': a++;
+            case '+':
+            case '-': a++;
+        }
+        switch (x) {
+            case '(': b++;
+            case '*':
+            case '/': b++;
+            case '+':
+            case '-': b++;
+        }
+
+        return a < b;
+    }
+    
+    bool is_empty() {
+        return list == NULL;
+    }
+};
+
